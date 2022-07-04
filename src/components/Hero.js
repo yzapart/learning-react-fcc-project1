4c6b953a60af80faf2bfdf47c4963f2b20f69c8d
yzapart <yohann.zapart@gmail.com>
2022-07-04 14:24:46 +0200

    Initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <section className="hero">
+      <img className="hero--photo" src="./images/photos-grid.png" alt="" />
+      <h1 className="hero--header">Online Experiences</h1>
+      <p className="hero--text">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas tenetur
+        qui assumenda ut eaque illum facere vel vero quos tempore!
+      </p>
+    </section>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <img className="nav--logo" src="./images/logo.png" alt="logo" />
+    </nav>
+  );
+}
